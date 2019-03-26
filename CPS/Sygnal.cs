e9a45d4313df364ea144ff68520252a6b34732d3
Małgorzata Kucharska <211964@edu.p.lodz.pl>
2019-03-26 18:08:36 +0100

    klasy sygnal i Sciagly

---
+++ CPS/Sygnal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPS
+{
+    public class Sygnal
+    {
+        int A; //amplituda
+        int t1; // czas poczatkowy
+        int d; // czas trwania
+        int t2 = t1 + d; //czas koncowy
+        int n; //numer probki
+        double T; //okres
+        double f = 1 / T; // czestotliwosc
+
+    }
+}
