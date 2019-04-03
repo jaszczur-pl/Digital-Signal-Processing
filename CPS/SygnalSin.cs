50f91a383e4084cb6b61af2585c359093c4b5669
Małgorzata Kucharska <211964@edu.p.lodz.pl>
2019-04-03 19:09:27 +0200

    sygnalSin

--- CPS/SygnalSin.cs
+++ CPS/SygnalSin.cs
@@ -8,9 +8,25 @@ namespace CPS
 {
     class SygnalSin:SCiagly
     {
+        double y;
         public SygnalSin(int A, int T) : base(A, T)
         {
 
+        }
+
+        public double FSin(int A, double T)
+        {
+            double x = A * Math.Sin((2*Math.PI)/T*(t1-t2));
+            return x;
+        }
+
+        public void PoliczWartoscY()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                y = FSin(A,T);
+                this.wartosciSygnaluY.Add(y);
+            }
         }
     }
 }
