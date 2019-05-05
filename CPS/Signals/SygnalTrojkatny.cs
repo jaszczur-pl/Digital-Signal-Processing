59a149b6116d3e7aa12d474ab457b916513b30f9
mjaszczura <maciej.jaszczura17@gmail.com>
2019-05-05 13:00:27 +0200

    Changed method of triangle signal generation

--- CPS/Signals/SygnalTrojkatny.cs
+++ CPS/Signals/SygnalTrojkatny.cs
@@ -22,27 +22,46 @@ namespace CPS
 
         public SygnalTrojkatny() { }
 
+        //public override void CalculateXYPoints() {
+        //    axisX.Clear();
+        //    axisY.Clear();
+
+        //    int z = 0;
+        //    double tempStartTime = t1;
+
+        //    for (double i = t1; i < (d + t1); i = i + (1 / f)) {
+        //        if (i >= tempStartTime + T) {
+        //            z++;
+        //            tempStartTime = i;
+        //        }
+        //        axisX.Add(i);
+
+        //        if (i >= (z * T + t1) && i < (kw * T + z * T + t1)) {
+        //            double temp2 = (A / (kw * T)) * (i - z * T - t1);
+        //            axisY.Add(temp2);
+        //        }
+        //        else {
+        //            double temp2 = (-A * (i - z * T - t1)) / (T * (1 - kw)) + A / (1 - kw);
+        //            axisY.Add(temp2);
+        //        }
+        //    }
+        //}
+
         public override void CalculateXYPoints() {
             axisX.Clear();
             axisY.Clear();
 
-            int z = 0;
-            double tempStartTime = t1;
-
             for (double i = t1; i < (d + t1); i = i + (1 / f)) {
-                if (i >= tempStartTime + T) {
-                    z++;
-                    tempStartTime = i;
-                }
+
+                double k = Math.Floor((i - t1) / T);
+
                 axisX.Add(i);
 
-                if (i >= (z * T + t1) && i < (kw * T + z * T + t1)) {
-                    double temp2 = (A / (kw * T)) * (i - z * T - t1);
-                    axisY.Add(temp2);
+                if (i < (k * T + t1) || i < (kw * T + k * T + t1) ){
+                    axisY.Add((A / (kw * T) * (i - k * T - t1)));
                 }
                 else {
-                    double temp2 = (-A * (i - z * T - t1)) / (T * (1 - kw)) + A / (1 - kw);
-                    axisY.Add(temp2);
+                    axisY.Add((-A / (T * (1 - kw)) * (i - k * T - t1) + A / (1 - kw)));
                 }
             }
         }
@@ -50,23 +69,16 @@ namespace CPS
         public override List<double> CalculateYPoints(List<double> axisX) {
 
             List<double> rAxisY = new List<double>();
-            int z = 0;
-            double tempStartTime = t1;
 
             foreach (var point in axisX) {
 
-                if (point >= tempStartTime + T) {
-                    z++;
-                    tempStartTime = point;
-                }
+                double k = Math.Floor((point - t1) / T);
 
-                if (point >= (z * T + t1) && point < (kw * T + z * T + t1)) {
-                    double temp2 = (A / (kw * T)) * (point - z * T - t1);
-                    rAxisY.Add(temp2);
+                if (point < (k * T + t1) || point < (kw * T + k * T + t1)) {
+                    rAxisY.Add((A / (kw * T) * (point - k * T - t1)));
                 }
                 else {
-                    double temp2 = (-A * (point - z * T - t1)) / (T * (1 - kw)) + A / (1 - kw);
-                    rAxisY.Add(temp2);
+                    rAxisY.Add((-A / (T * (1 - kw)) * (point - k * T - t1) + A / (1 - kw)));
                 }
             }
 
